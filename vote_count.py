2f64118d1ac80148bc67c782eb59bacf423e00fe
Alexandre Teles <alexandre.teles@ufba.br>
2023-03-26 18:44:17 -0300

    feat: add vote counting script

--- vote_count.py
+++ vote_count.py
@@ -1,7 +1,9 @@
 import os
 import redis
 import requests
-from pprint import pprint
+from pprint import pformat
+
+paste_url: str = "https://api.paste.ee/v1/pastes"
 
 db = redis.StrictRedis(
     host=os.environ["REDIS_URL"], port=os.environ["REDIS_PORT"], db=3
@@ -18,11 +20,40 @@ for key in db.scan_iter("*"):
             else:
                 votes[entry["cid"]] = 1
 
-raw_votes: str = pprint.pformat(votes)
-sorted_votes: str = pprint.pformat(
-    dict(sorted(votes.items(), key=lambda item: item[1], reverse=True))
-)
+raw_votes: str = pformat(votes)
+sorted_votes: str = pformat(sorted(votes))
+
+text: list = [
+        'Raw votes:\n\n',
+        raw_votes,
+        '\n\nSorted votes:\n\n',
+        sorted_votes
+]
 
 with open("votes.txt", "w") as f:
-    f.write(raw_votes)
-    f.write(sorted_votes)
+    f.writelines(text)
+
+payload: dict = {
+    "description": "ReVanced Poll Results",
+    "sections": [
+        {
+            "name": "Raw Votes",
+            "syntax": "autodetect",
+            "contents": raw_votes
+        },
+        {
+            "name": "Sorted Votes",
+            "syntax": "autodetect",
+            "contents": sorted_votes
+        }
+    ]
+}
+
+headers = {
+    "Content-Type": "application/json",
+    "X-Auth-Token": os.environ["PASTE_EE_KEY"]
+}
+
+response: dict = requests.post(paste_url, json=payload, headers=headers).json()
+
+print(f'Upload response: {response["link"]}')
